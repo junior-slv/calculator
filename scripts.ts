c1fb7ae8f40dc4d7494395d98a41591d929d9b0c
Junior <dirceusilva0422@gmail.com>
2022-11-06 02:53:37 -0300

    new functions

---
+++ scripts.ts
@@ -0,0 +1,21 @@
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
+const less = document.getElementById('less');
+const plus = document.getElementById('plus');
+const clear = document.getElementById('clear');
+const divided = document.getElementById('divided');
+const mult = document.getElementById('mult');
+const equal = document.getElementById('equal');
+
+var number1 = [];
+var number2 = [];
+
+
